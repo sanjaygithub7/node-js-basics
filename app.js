b8c7d148a2af57fe03dff094080566bbc079137f
sanjaygithub7 <sanjaykumar712121@gmail.com>
2024-09-17 19:38:45 +0530

    converted into npm package

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const http=require('http')     // this import the http module from core module of nodejs
+// simple example of getting input data from request storing in text
+
+const http=require('http')   
+const requesthandler=require('./routes')
  
-const server=http.createServer((req,res)=>{    // this method 'create server' creates a server and function is given for handling the request and response
+const server=http.createServer((requesthandler))   
 
-    
-    // creating html response to browser request
-    res.setHeader('content-type','text/html')     //the response should be in html code         
-    res.write('<html>')
-    res.write('<h1>Hello from Nodejs Server</h1>')
-    res.write('</html>')
-    res.end()
-})     
-server.listen(5000)                          // creating a port  where requests and responses are sent and received in a network
 
+server.listen(5000) 
